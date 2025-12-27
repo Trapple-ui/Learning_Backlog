9e917a4c99b7f208894591cce5abca32588c9030
Damir <ds@gmail.com>
2025-12-28 00:19:03 +0500

    Add ResourceTags table (not tested)

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import sqlalchemy as db
-from sqlalchemy import CheckConstraint
+from sqlalchemy import CheckConstraint, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 engine = db.create_engine('postgresql+psycopg2:///learning_tracker.db')
@@ -7,7 +7,7 @@ engine = db.create_engine('postgresql+psycopg2:///learning_tracker.db')
 class Model(DeclarativeBase): pass
 
 class Tags(Model):
-    __tablename__ = 'Tags'
+    __tablename__ = 'tags'
 
     tag_id: Mapped[int] = mapped_column(primary_key=True)
     tag_name: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
@@ -28,3 +28,11 @@ class Resources(Model):
         CheckConstraint(type.in_(('видео', 'статья', 'курс', 'книга', 'сайт', 'подкаст')), name='check_in_type'),
         CheckConstraint(priority.in_((1, 2, 3, 4, 5)), name='check_in_priority')
     )
+
+
+class ResourceTags(Model):
+    __tablename__ = 'resource_tags'
+
+    # первичный ключ - это составной ключ из таблицы ресурсов и тегов
+    tag: Mapped[int] = mapped_column(db.Integer, ForeignKey('tags.tag_id'), primary_key=True)
+    res_id: Mapped[int] = mapped_column(db.Integer, ForeignKey('resources.res_id'), primary_key=True)
